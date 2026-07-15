d6046c08ab12392d2ec820d5539248da1fc432ad
mhr271 <humaidirahman271@gmail.com>
2026-07-15 18:49:20 +1000

    Update model.py

--- model.py
+++ model.py
@@ -10,3 +10,10 @@ from sklearn.linear_model import LogisticRegression
 import xgboost as xgb
 from xgboost import XGBClassifier
 
+
+df = pd.read_csv("WA_Fn-UseC_-Telco-Customer-Churn.csv")
+print(df.head())
+print(df.shape)
+
+y = df["Churn"]
+x = df.drop("churn",axis=1)
